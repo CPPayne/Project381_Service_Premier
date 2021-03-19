cf07917c0fe882ae36e3aff4de0110fda542e74c
Charles Payne <charlpayne1@gmail.com>
2021-03-19 11:46:22 +0200

    All Classes, Except Daniel's

---
+++ Project381_Service_Premier/Technician.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Wian_Se_Gedeelte_classes_
+{
+    class Technician
+    {
+        private string expertise;
+
+        public Technician(string expertise)
+        {
+            this.Expertise = expertise;
+        }
+
+        public string Expertise { get => expertise; set => expertise = value; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Technician technician &&
+                   Expertise == technician.Expertise;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Expertise);
+        }
+    }
+}
--- Project381_Service_Premier/WorkRequest.cs
+++ Project381_Service_Premier/WorkRequest.cs
@@ -6,22 +6,32 @@ namespace Project_Wian_Se_Gedeelte_classes_
 {
     class WorkRequest
     {
-        public int id { get; set; };
-        public string discription { get; set; };
-        public string expertise_requered { get; set; };
+        private int id;
+        private string description;
+        private string expertise_required;
 
-        public void Equals(int id, string discription, string expertise_requered)
+        public WorkRequest(int id, string description, string expertise_required)
         {
-            return id;
-            return discription;
-            return expertise_requered;
+            this.Id = id;
+            this.Description = description;
+            this.Expertise_required = expertise_required;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Description { get => description; set => description = value; }
+        public string Expertise_required { get => expertise_required; set => expertise_required = value; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is WorkRequest request &&
+                   id == request.id &&
+                   description == request.description &&
+                   expertise_required == request.expertise_required;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Equals(id, discription, expertise_requered);
+            return HashCode.Combine(id, description, expertise_required);
         }
-
-
     }
 }
