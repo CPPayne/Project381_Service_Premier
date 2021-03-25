37dd8f40bc2a075f5ad3edecb99f6433e728afef
Charles Payne <charlpayne1@gmail.com>
2021-03-25 09:21:24 +0200

    Some Database connections and operations structure

--- Project381_Service_Premier/FileHandler.cs
+++ Project381_Service_Premier/FileHandler.cs
@@ -8,14 +8,149 @@ namespace ConsoleApp1
    {
       public FileHandler()
       {
-
-
       }
 
-      public void GetClientHistory()
+       //Constructor
+        public DataHandler()
+        { }
+
+        //Set connection string
+        string connect = "Data Source=.; Initial Catalog= premierServiceDB; Integrated Security= SSPI";
+        SqlConnection conn;     //Declare SqlConnection object
+        SqlCommand command;     //Declare SqlCommand object
+        SqlDataReader reader;   //Declare SqlDataReader object
+
+        //Declare Student object
+        Student objStudent = new Student();
+
+        //Register method
+        public void Register(int sID, string sName, string sSurnane, string cID) 
+        {
+            string query = @"INSERT INTO Students VALUES ( '" + sID + "', '" + sName + "', '" + sSurnane + "', '" + cID + "' )";
+
+            conn = new SqlConnection(connect);
+
+            conn.Open();
+
+            command = new SqlCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Details of new student saved");
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Details of new student not saved: " + ex.Message);
+            }
+            finally 
+            {
+                conn.Close();
+            }
+        }
+
+        //Delete method
+        public void Delete(int sID)
+        {
+            string query = @"DELETE FROM Students WHERE StudentID = ('"+ sID +"')";
+
+            conn = new SqlConnection(connect);
+
+            conn.Open();
+
+            command = new SqlCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Deleted the details of student with Student Number: " + sID);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        //Search method
+        public List<Student> Search(int sID)
+        {
+            //query select all collumns provided stdID = value
+
+            string query = @"SELECT * FROM Students WHERE StudentID = ('" + sID + "')";
+
+            //connect
+            conn = new SqlConnection(connect);
+
+            conn.Open();
+
+            command = new SqlCommand(query, conn);
+            List<Student> myStudent = new List<Student>();
+
+            //run query command
+            try
+            {
+                //read data SqlDataReader
+                reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    //Store each collumn value in student class variables/field
+                    objStudent.StudentID = int.Parse(reader[0].ToString());
+                    objStudent.StudentName = reader[1].ToString();
+                    objStudent.StudentSurname = reader[2].ToString();
+                    objStudent.CourseID = reader[3].ToString();
+
+                    //Add field values to a Student type list
+                    myStudent.Add(new Student(objStudent.StudentID, objStudent.StudentName, objStudent.StudentSurname, objStudent.CourseID));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            //Return the student list
+            return myStudent;
+        }
+
+        //Update method
+        public void Update(int sID, string sName, string sSurnane, string cID)
+        {
+            string query = @"UPDATE Students SET StudentID = ('" + sID + "'), FirstName = ('"+ sName + "'), " +
+                "LastName = ('"+ sSurnane + "'), CourseID = ('"+ cID + "') WHERE StudentID = ('"+ sID + "')";
+
+            conn = new SqlConnection(connect);
+
+            conn.Open();
+
+            command = new SqlCommand(query, conn);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Details updated");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+      public List<Call> GetClientCallHistory()
       {
 
       }
+
       public void GetClientWorkHistory() { }
 
       public void GetTechnicianWorkHistory() { }
