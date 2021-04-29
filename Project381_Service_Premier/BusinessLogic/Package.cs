2c68a2cdb8f15365ac2b7655ef783fcab2a3b5c2
IgnatioFerreira <ignatio.ferreira@gmail.com>
2021-04-29 10:15:00 +0200

    Conflict test

--- Project381_Service_Premier/BusinessLogic/Package.cs
+++ Project381_Service_Premier/BusinessLogic/Package.cs
@@ -11,24 +11,24 @@ namespace Taak
       private double cost;
       private DateTime startDate;
       private int duration;
-      private Boolean isBusiness;
+      private List<Service> Services;
 
-      public Package(string packagename, int lvl, double Cost, DateTime StartDate, int Duration, Boolean IsBusiness)
+      public Package(string packagename, int lvl, double Cost, DateTime StartDate, int Duration, List<Service> Services)
       {
          this.PackageName = packagename;
          this.Level = lvl;
          this.Cost = Cost;
          this.StartDate = StartDate;
          this.Duration = Duration;
-         this.IsBusiness = IsBusiness;
-      }
+         this.Services1 = Services;
+      }             //ek hou van appels
 
       public string PackageName { get => packageName; set => packageName = value; }
       public int Level { get => level; set => level = value; }
       public double Cost { get => cost; set => cost = value; }
       public DateTime StartDate { get => startDate; set => startDate = value; }
       public int Duration { get => duration; set => duration = value; }
-      public Boolean IsBusiness { get => isBusiness; set => isBusiness = value; }
+      internal List<Service> Services1 { get => Services; set => Services = value; }
 
       public override bool Equals(object obj)
       {
