985534f3ebbc7ec481fb06fe55b140f0e1663375
Charles Payne <charlpayne1@gmail.com>
2021-05-10 17:52:58 +0200

    some more fixes

--- Project381_Service_Premier/BusinessLayer/Schedule.cs
+++ Project381_Service_Premier/BusinessLayer/Schedule.cs
@@ -7,23 +7,14 @@ namespace Project381_Service_Premier.BusinessLayer
 {
    class Schedule
    {
-      private TimeSpan duration;
-      private DateTime appointmentStart;
-      private Client client;
-      private Technician technician;
-      private int scheduleID;
-      private int day;
-      private int buffer;
+        private TimeSpan duration;
+        private DateTime appointmentStart;
+        private Client client;
+        private Technician technician;
+        private int scheduleID;
+        private int day;
+        private int buffer;
 
-        public TimeSpan Duration { get => duration; set => duration = value; }
-        public DateTime AppointmentStart { get => appointmentStart; set => appointmentStart = value; }
-        internal Client Client { get => client; set => client = value; }
-        internal Technician Technician { get => technician; set => technician = value; }
-        public int ScheduleID { get => scheduleID; set => scheduleID = value; }
-        public int Day { get => day; set => day = value; }
-        public int Buffer { get => buffer; set => buffer = value; }
-
-        public Schedule() { }
 
         public Schedule(TimeSpan duration, DateTime appointmentStart, Client client, Technician technician, int scheduleID, int day, int buffer)
         {
@@ -35,6 +26,18 @@ namespace Project381_Service_Premier.BusinessLayer
             this.Day = day;
             this.Buffer = buffer;
         }
+
+        public TimeSpan Duration { get => duration; set => duration = value; }
+        public DateTime AppointmentStart { get => appointmentStart; set => appointmentStart = value; }
+        internal Client Client { get => client; set => client = value; }
+        internal Technician Technician { get => technician; set => technician = value; }
+        public int ScheduleID { get => scheduleID; set => scheduleID = value; }
+        public int Day { get => day; set => day = value; }
+        public int Buffer { get => buffer; set => buffer = value; }
+
+        public Schedule() { }
+
+
 
         public void CalcualteAppointmentTime()
       {
@@ -87,37 +90,7 @@ namespace Project381_Service_Premier.BusinessLayer
       }
 
 
-      private int day;
-      private int buffer;
-
-      private void SortSchedules()
-      {
-         FileHandler fh = new FileHandler();
-         List<Schedule> AllSchedules = new List<Schedule>();
-         for (int i = 0; i < 50; i++)
-         {
-            List<Schedule> tempSchedules = new List<Schedule>();
-            foreach (Schedule schedule in AllSchedules)
-            {
-               if (schedule.day == i)
-               {
-                  tempSchedules.Add(schedule);
-                  AllSchedules.Remove(schedule);
-               }
-            }
-            tempSchedules.Sort((x, y) => x.buffer.CompareTo(y.buffer));
-
-            while (tempSchedules.Count > 6)
-            {
-               tempSchedules[tempSchedules.Count - 1].day++;
-               tempSchedules[tempSchedules.Count - 1].buffer--;
-               //verify buffer > 0
-               fh.IncrementDayDecrementBuffer();
-            }
-            AllSchedules.AddRange(tempSchedules);
-         }
-         AllSchedules.Sort((x, y) => x.day.CompareTo(y.day));
-      }
+   
 
    }
 }
